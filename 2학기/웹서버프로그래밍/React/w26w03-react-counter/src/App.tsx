79434944f893f4bd883823460eecc4c77f8cd39b
I_Like_Cyberpunk <gtaeho85@gmail.com>
2026-09-19 04:34:48 +0900

    r

---
+++ 2학기/웹서버프로그래밍/React/w26w03-react-counter/src/App.tsx
@@ -0,0 +1,24 @@
+import './App.css'
+import { useState } from 'react';
+
+function App(){
+
+  const [count, setCount] = useState(0);
+  // const plusOne = (prev: number) => prev +1;
+  
+  return (
+    <>
+      <h1>{count}</h1>
+      <button onClick={()=>{
+        setCount(A => A+5)
+      } }>
+        증가
+      </button>
+    </>
+  )
+}
+
+export default App
+
+
+
